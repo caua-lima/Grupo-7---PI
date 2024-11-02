acb1ca75775ae5d619222f6fda5fda5db2bc96ba
marco bubola <marcobubola@hotmail.com>
2024-11-02 11:21:21 -0300

    atualizacoes

--- piPADRAO/conexao.php
+++ piPADRAO/conexao.php
@@ -3,13 +3,11 @@
 $host = "localhost";
 $dbname = "bancopi";
 $username = "root";
-$password = "fatec";
+$password = "12345";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e){
+} catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
-
-?>
