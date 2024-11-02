bb393514a27dea758b84ec4a39148ca6fbf503f4
Joaoouza <136836692+Joaoouza@users.noreply.github.com>
2024-11-02 16:09:04 -0300

    Update and rename home.html to home.php

--- piPADRAO/home.php
+++ piPADRAO/home.php
@@ -8,7 +8,7 @@
     <script src="https://kit.fontawesome.com/f3cc8687d6.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header>
+    <header> <!-- Inicio Cabeçalho -->
         <div class="containerh">
                 <div class="first-columh">
                         <a class="link-sp" href="#">
@@ -46,11 +46,58 @@
                     </div>
                 </div>
         </div>
-    </header>
+    </header> 
     <!-- Fim cabeçalho -->
 
-    <div class="container">
-        
+    <!-- Sub Cabeçalho -->
+    <div class="container-sc">
+        <div class="first-column-sc">
+            <a href="#">
+                <img class="logo-ita" src="img/logo-fatec_itapira.png" alt="">
+            </a>
+            <a href="#">
+                <img class="logo-cps" src="img/logo-cps.png" alt="">
+            </a>
+        </div>
+        <div class="second-column-sc">
+            <h2 class="title">Programa para Ausências Docentes</h2>
+            <h2 class="title">e Reposição de Aulas Oficiais</h2>
+        </div>
+        <div class="third-column-sc">
+            <img class="logo-padrao" src="img/logo-padrao.png" alt="">
+            <button class="btn">SAIR</button>
+        </div>
+    </div>
+
+    <div class="container-geral">
+        <div class="content">
+            <a class="link-column" href="faltas.php">
+                <div class="first-column">
+                        <ul class="list-window">
+                            <li class="item-window"><i class="fa-solid fa-clipboard-list"></i></li>
+                            <li class="desc-window"><p>FORMULÁRIO</p></li>
+                            <li class="desc-window"><p>DE FALTAS</p></li>
+                        </ul>
+                </div>
+            </a>
+            <a class="link-column" href="reposicao.php">
+                <div class="second-column">
+                    <ul class="list-window">
+                        <li class="item-window"><i class="fa-regular fa-calendar-days"></i></li>
+                        <li class="desc-window"><p>REPOSIÇÃO</p></li>
+                        <li class="desc-window"><p>DE AULAS</p></li>
+                    </ul>
+                </div>
+            </a>
+            <a class="link-column" href="professor.php">
+                <div class="third-column">
+                    <ul class="list-window">
+                        <li class="item-window"><i class="fa-regular fa-clock"></i></li>
+                        <li class="desc-window"><p>HISTÓRICO</p></li>
+                    </ul>
+                </div>
+            </a>
+        </div>
     </div>
 
     <footer>
